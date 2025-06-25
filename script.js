45ded3af4ec3b4b43495880ef0647448ea2d5e6a
emmanuelybrandao <hyuugapft@gmail.com>
2025-06-25 20:19:49 -0300

    acrescentando os arquivos css, html e js

---
+++ script.js
@@ -0,0 +1,3 @@
+alert('Coomando para mostra um texto na tela, posso usar aspas duplas ou aspas simples, siga este padrão em todo o codigo');
+alert('ele é limitado ao clique no ok pelo usuario');
+alert('Não é possivel configurar seu tempo de permanencia na tela');
